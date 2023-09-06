fae9b726fdcdcec32177e8cba13438c4264b0eef
Mochamad Fadlan Abdillah <mfadlanabdillah@outlook.com>
2023-09-07 02:10:01 +0700

    [NEW] Added UI using react-bootstrap and RESTful API endpoints based on the db.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,116 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import AddNameForm from "./components/AddNameForm";
+import NameList from "./components/NameList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [names, setNames] = useState([]);
+  const [editIndex, setEditIndex] = useState(null); // Initialize with null
+
+  useEffect(() => {
+    // Fetch the names from json-server when the component mounts
+    fetch("http://localhost:3001/preman")
+      .then((response) => response.json())
+      .then((data) => {
+        setNames(data);
+      });
+  }, []); // Empty dependency array to run this effect once
+
+  const handleAddName = (newName) => {
+    if (editIndex !== null) {
+      // Edit existing name
+      const updatedNames = names.map((name) =>
+        name.id === editIndex ? { ...name, name: newName } : name
+      );
+      setNames(updatedNames);
+      setEditIndex(null);
+
+      // Send a PUT request to update the name on the JSON Server
+      fetch(`http://localhost:3001/preman/${editIndex}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: newName }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to update name on the server.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error updating name:", error);
+        });
+    } else {
+      // Add new name
+      fetch("http://localhost:3001/preman", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: newName }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setNames([...names, data]);
+        })
+        .catch((error) => {
+          console.error("Error adding name:", error);
+        });
+    }
+  };
+
+  const handleEditName = (id) => {
+    setEditIndex(id);
+  };
+
+  const handleDeleteName = (id) => {
+    // Send a DELETE request to remove the name from JSON Server
+    fetch(`http://localhost:3001/preman/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          // If successful, update the state to reflect the change
+          const updatedNames = names.filter((name) => name.id !== id);
+          setNames(updatedNames);
+        } else {
+          throw new Error("Failed to delete name on the server.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting name:", error);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container>
+      <Row>
+        <Col>
+          <h1>
+            {editIndex !== null
+              ? "Edit Barudak Preman Member"
+              : "Barudak Preman Member"}
+          </h1>
+          <AddNameForm
+            onAddName={handleAddName}
+            editIndex={editIndex}
+            names={names}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1>Name List</h1>
+          <NameList
+            names={names}
+            onEditName={handleEditName}
+            onDeleteName={handleDeleteName}
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+root.render(
   <div>
     <App />
   </div>
